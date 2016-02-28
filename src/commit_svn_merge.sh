b953023ee190c3ed542888de39f811932e7d4f98
Juan Jose Pablos <juanjo@apertus.es>
2016-02-28 16:13:19 +0100

    Scripts e importacion que no deberian de estar aqui

--- src/commit_svn_merge.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-svn up
-svn -r $(cat last_merged_version):HEAD merge http://svn.infosial-dmz.lan/abanq/motor/2.5/developer/src
-svn commit -m ""
-svn up
-echo $(svnversion -n) > last_merged_version
-svn commit -m ""
-
