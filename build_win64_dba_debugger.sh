9801ca713fda2fd82886fb7ff0f70bcc095bbaa4
José A. Fernández <aullasistemas@gmail.com>
2018-07-30 11:17:53 +0200

    build debugger

---
+++ build_win64_dba_debugger.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export CROSS='x86_64-w64-mingw32-'
+
+./build.sh -prefix 'C:\eneboo' -platform 'win32-g++-cross' -dbadmin -win64 -fixxp -debugger && ./clean_build.sh "eneboo-build-win64-dba"
+
