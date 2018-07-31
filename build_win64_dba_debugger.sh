5a084f10b18806c441c0a755acc57f3b74f16c3e
usuario <usuario@compilator.(none)>
2018-07-31 12:30:29 +0200

    Se añade modificador gdb

--- build_win64_dba_debugger.sh
+++ build_win64_dba_debugger.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export CROSS='x86_64-w64-mingw32-'
 
-./build.sh -prefix 'C:\eneboo' -platform 'win32-g++-cross' -dbadmin -win64 -fixxp -debugger && ./clean_build.sh "eneboo-build-win64-dba"
+./build.sh -prefix 'C:\eneboo' -platform 'win32-g++-cross' -dbadmin -win64 -fixxp -debugger -gdb && ./clean_build.sh "eneboo-build-win64-dba"
 
