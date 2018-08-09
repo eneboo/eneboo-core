b39f6aa200d7bde37c7578e3b3267de24b118eb8
José A. Fernández <aullasistemas@gmail.com>
2018-08-09 16:37:42 +0200

    Bugfix: Algunas formRecordDB no recogen un parent vátido y crashea.rc7

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DIR="$( cd -P "$( dirname "$0" )" && pwd )"
 cd "$DIR"
-VER="2.4.6-RC6"
+VER="2.4.6-RC7"
 
 REBUILD_QT=auto
 OPT_PREFIX=""
--- src/flbase/FLSqlCursor.cpp
+++ src/flbase/FLSqlCursor.cpp
@@ -1705,14 +1705,16 @@ void FLSqlCursor::openFormInMode(int m, bool cont)
   if (!refreshBuffer())
     return;
 
-  QWidget *activeWid = aqApp->activeModalWidget();
+  QWidget *activeWid = aqApp->mainWidget();
+  if (!activeWid)
+      aqApp->activeModalWidget();
   if (!activeWid)
       activeWid = aqApp->activePopupWidget();
   if (!activeWid)
       activeWid = aqApp->activeWindow(); 
   
     if (activeWid)
-	qWarning(tr("FLSqlCursor::openFormInMode :: ActiveW = %1").arg(activeWid->name()));
+	qWarning(tr("FLSqlCursor::openFormInMode :: ActiveW = %1 Action = %2").arg(activeWid->name(), d->action_->name()));
 
   FLFormRecordDB *f = new FLFormRecordDB(this, d->action_->name(),
                                          activeWid , cont);
