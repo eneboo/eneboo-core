bb6bf5f28b183870fa251c236417d57f800d0a4f
José A. Fernández <aullasistemas@gmail.com>
2020-11-29 19:22:11 +0100

    prueba 30

--- tools/aqods/AQOdsRow.h
+++ tools/aqods/AQOdsRow.h
@@ -93,8 +93,7 @@ private:
       return v;
     QRegExp rx("((([A-Za-z]{3,9}:(?:\\/\\/)?)(?:[\\-;:&=\\+\\$,\\w]+@)?[A-Za-z0-9\\.\\-]+|(?:www\\.|[\\-;:&=\\+\\$,\\w]+@)[A-Za-z0-9\\.\\-]+)((?:\\/[\\+~%\\/\\.\\w\\-]*)?\\??(?:[\\-\\+=&;%@\\.\\w]*)#?(?:[\\.\\!\\/\\\\\\w]*))?)");
     QString theUrl(v);
-    //return theUrl.replace(rx, "__HREF1__='\\1__HREF2__\\1__HREF3__");
-    return theUrl;
+    return theUrl.replace(rx, "__HREF1__='\\1__HREF2__\\1__HREF3__");
   }
 };
 
