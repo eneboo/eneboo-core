a2b20b536c96c74893bbdee0d7344058d33b0c13
José A. Fernández <aullasistemas@gmail.com>
2020-11-29 19:57:35 +0100

    prueba 38

--- tools/aqods/AQOdsGenerator.cpp
+++ tools/aqods/AQOdsGenerator.cpp
@@ -812,7 +812,7 @@ bool AQOdsGenerator::generateOds(const QString &fileNameOut)
     
     while (cntAux.find("office:value=\"__HREF1__") > -1) {
     	int pos_inicial = cntAux.find("office:value=\"__HREF1__");
-    	int pos_final = cntAux.find("__HREF3__\"");
+    	int pos_final = cntAux.find("__HREF3__");
     	cntAux.replace(pos_inicial, pos_inicial - pos_final , "");
     }
     
