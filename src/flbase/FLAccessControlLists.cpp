6931d76da5a5308d8127c414d0de3a2feb1b94fd
José A. Fernández <aullasistemas@gmail.com>
2020-12-07 09:55:58 +0100

    prueba4

--- src/flbase/FLAccessControlLists.cpp
+++ src/flbase/FLAccessControlLists.cpp
@@ -120,14 +120,15 @@ void FLAccessControlLists::process(QObject *obj)
   if (!acls_list_) {
   	acls_list_ = new QDict<QString>(2);
   	}
+  	
    acls_list_.clear();
   
-  acls_list << type + QString::fromLatin1("::") + name + QString::fromLatin1("::") + user;
+  acls_list_ << type + QString::fromLatin1("::") + name + QString::fromLatin1("::") + user;
   if (type == "mainwindow") {
-  	acls_list << type + QString::fromLatin1("::container::") + user;
+  	acls_list_ << type + QString::fromLatin1("::container::") + user;
   }
     
-  QDictIterator < QString > it(*acls_list);
+  QDictIterator < QString > it(*acls_list_);
   QString acl_name = "";
   
   for (; it.current(); ++it) {
