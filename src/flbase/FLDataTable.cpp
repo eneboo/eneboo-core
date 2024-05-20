dfdec5dae4d7f4b1a83c5f595f03957672559d94
José A. Fernández <aullasistemas@gmail.com>
2024-05-20 12:31:34 +0200

    ajustes enconde

--- src/flbase/FLDataTable.cpp
+++ src/flbase/FLDataTable.cpp
@@ -12,8 +12,8 @@ email                : mail@infosial.com
  ***************************************************************************/
 /***************************************************************************
    Este  programa es software libre. Puede redistribuirlo y/o modificarlo
-   bajo  los  tï¿½rminos  de  la  Licencia  Pï¿½blica General de GNU   en  su
-   versiï¿½n 2, publicada  por  la  Free  Software Foundation.
+   bajo  los  términos  de  la  Licencia  Pública General de GNU   en  su
+   versión 2, publicada  por  la  Free  Software Foundation.
  ***************************************************************************/
 
 #include "FLDataTable.h"
@@ -773,7 +773,7 @@ void FLDataTable::paintField(QPainter *p, const QSqlField *field,
     }
     else
     {
-      text = field->value().toBool() ? tr("Sï¿½") : tr("No");
+      text = field->value().toBool() ? tr("Sí") : tr("No");
       p->drawText(2, 2, cr.width() - 4, cr.height() - 4,
                   fieldAlignment(field), text);
     }
