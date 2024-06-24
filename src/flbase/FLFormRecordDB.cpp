1fae6ebc3bf9d5b42a5c6c28af66b8e4bea6a80f
José A. Fernández <aullasistemas@gmail.com>
2024-06-24 10:45:28 +0200

    ajustes checkPushbuttonsAccept

--- src/flbase/FLFormRecordDB.cpp
+++ src/flbase/FLFormRecordDB.cpp
@@ -346,8 +346,13 @@ void FLFormRecordDB::setMainWidget(QWidget *w)
     {
       pushButtonAccept = new QPushButton(this, "pushButtonAccept");
       connect(pushButtonAccept, SIGNAL(clicked()), this, SLOT(accept()));
-      connect(cursor_, SIGNAL(bufferChanged()), this, SLOT(checkPushButtonsAccept()));
     }
+
+    qWarning("******************** ACTIVANDO *******************");
+    disconnect(cursor_, SIGNAL(bufferChanged()), this, SLOT(checkPushButtonsAccept()));
+    connect(cursor_, SIGNAL(bufferChanged()), this, SLOT(checkPushButtonsAccept()));
+
+
     pushButtonAccept->setSizePolicy(QSizePolicy((QSizePolicy::SizeType)0,
                                                 (QSizePolicy::SizeType)0, 0, 0,
                                                 pushButtonAccept->sizePolicy().hasHeightForWidth()));
