81065d3ef09ec41df90c9a575cdcb0bc13233dcf
José A. Fernández <aullasistemas@gmail.com>
2024-06-26 12:03:10 +0200

    eliminar debug

--- src/flbase/FLSqlQuery.cpp
+++ src/flbase/FLSqlQuery.cpp
@@ -244,13 +244,10 @@ bool FLSqlQuery::isNull(const QString &n) const
 
 QVariant FLSqlQuery::value(int i, bool raw) const
 {
-  qWarning("VALUE DE " + QString::number(i));
   if (!isValid() && i >= 0)
     return QVariant();
   QVariant v(QSqlQuery_value(i));
   QVariant::Type type = v.type();
-  qWarning("TYPE " + QString::number(type) + ", type string =" + QString::number(QVariant::String));
-  qWarning("value" + v.toString());
 
   if (!raw  && type == QVariant::String) {
     if (!v.isNull()) {
