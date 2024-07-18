1552b3eeb231b2c0ec14b623d06594bc6a0e5d58
José A. Fernández <aullasistemas@gmail.com>
2024-07-18 10:57:05 +0200

    fix

--- src/sqlapi/sqlitedataset.cpp
+++ src/sqlapi/sqlitedataset.cpp
@@ -741,7 +741,7 @@ void SqliteDataset::fill_fields()
   int SqliteDataset::exec(const string &sql)
   {
     if (debug_sql) {
-      qWarning("EXEC ! --> %s", sql);
+      qWarning("EXEC ! --> " + sql);
     }
     if (!handle()) return DB_ERROR;
     int res;
