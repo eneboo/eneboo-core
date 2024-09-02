f8e7e0d999c2c4e6566d3a85fa60800ee63db07f
José A. Fernández <aullasistemas@gmail.com>
2024-09-02 11:31:22 +0200

    canUnaccent(12)

--- src/plugins/sqldrivers/sqlapi/sqlapi.h
+++ src/plugins/sqldrivers/sqlapi/sqlapi.h
@@ -86,7 +86,7 @@ public:
   void Mr_Proper();
 
   bool existsTable(const QString &n) const;
-  bool canUnaccent() const;
+  bool canUnaccent();
   bool disabled_transaction_;
   QProcess *AQProc;
 
