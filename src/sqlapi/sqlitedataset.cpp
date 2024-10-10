c034492d7db15a88d7e9c7019adbfc04a1f493d1
José A. Fernández <aullasistemas@gmail.com>
2024-10-10 19:30:32 +0200

    ajustes compilación driver sqlite3

--- src/sqlapi/sqlitedataset.cpp
+++ src/sqlapi/sqlitedataset.cpp
@@ -211,7 +211,7 @@ namespace dbiplus
   int SqliteDatabase::connect()
   {
     disconnect();
-    int result = sqlite3_open_v2(db.c_str(), &conn, SQLITE_OPEN_NOMUTEX); // FULLMUTEX serializa el multithread .... :(
+    int result = sqlite3_open_v2(db.c_str(), &conn, SQLITE_OPEN_NOMUTEX, 0); // FULLMUTEX serializa el multithread .... :(
     //char* err=NULL;
     if (result != SQLITE_OK) {
       return DB_CONNECTION_NONE;
