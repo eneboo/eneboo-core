a5d26a4360158a7256d726518e0d9d7e0ed09e74
José A. Fernández <aullasistemas@gmail.com>
2024-10-24 13:00:34 +0200

    buscando refresh

--- src/flbase/FLFieldDB.cpp
+++ src/flbase/FLFieldDB.cpp
@@ -2721,7 +2721,9 @@ void FLFieldDB::toggleAutoCompletion()
       if (frameWidth < autoComFrame_->width())
         frameWidth = autoComFrame_->width();
       autoComFrame_->setGeometry(tmpPoint.x(), tmpPoint.y(), frameWidth, 300);
+      qWarning("P3");
       autoComFrame_->show();
+      qWarning("P4");
       autoComFrame_->setFocus();
     } else if (autoComFrame_->isVisible() && cur->size() == 1)
       autoComFrame_->hide();
