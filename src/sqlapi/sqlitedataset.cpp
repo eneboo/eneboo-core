f5c72d80f7c46e7626339b0f84771cd3d3c9b556
José A. Fernández <aullasistemas@gmail.com>
2024-10-24 12:10:44 +0200

    activado debug paginacion

--- src/sqlapi/sqlitedataset.cpp
+++ src/sqlapi/sqlitedataset.cpp
@@ -1172,13 +1172,8 @@ bool SqliteDataset::procesa_datos_cadena_recibida(const QString &salida, const i
 
     if (lista_size > 0 && lista_size != cabecera_size) {
       qWarning("Error de integridad de datos. El número de columnas no coincide. offset:" + QString::number(offset) + ", linea: " + QString::number(pos) + ", Cabecera: " + QString::number(cabecera_size) + ", Valores: " + QString::number(lista_size));
-      if (debug_paginacion) {
-        qWarning("Registro: " + registro);
-      }
-      for (int x = 0; x < result.record_header.size(); x++) {
-        //qWarning("Col : " + QString::number(x) + " : " + result.record_header[x].name + x == 0 ? "(* Omitida)": "");
-      }
-      return false;
+      pos++;
+      continue;
     }
 
     //qWarning("PROCESANDO VALORES LINEA N? %d", sz);
