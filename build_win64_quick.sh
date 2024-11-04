be45b6de55f761ede9a094ec65b0a07befa99eaf
José A. Fernández <aullasistemas@gmail.com>
2024-11-04 09:16:37 +0100

    argumentos en builds por defecto

--- build_win64_quick.sh
+++ build_win64_quick.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export CROSS='x86_64-w64-mingw32-'
 
-./build.sh -prefix 'C:\eneboo' -platform 'win32-g++-cross' -win64 -quick && ./clean_build.sh "eneboo-build-win64-quick"
+./build.sh -prefix 'C:\eneboo' -platform 'win32-g++-cross' -win64 -quick -sqllog -gdb && ./clean_build.sh "eneboo-build-win64-quick"
 
