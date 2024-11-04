f7aa34ee2f41b9cb483385066bccb0a797a1d1d2
José A. Fernández <aullasistemas@gmail.com>
2024-11-04 11:25:54 +0100

    eliminado debug

--- src/plugins/sqldrivers/sqlite/qsqlite.cpp
+++ src/plugins/sqldrivers/sqlite/qsqlite.cpp
@@ -1012,7 +1012,7 @@ QVariant SqliteResult::data(int i)
 
   QString valorUTF8 = dataSet->fv(dataSet->fieldName(i)).get_asString();
   QString valorLatin1 = QString::fromUtf8(valorUTF8);
-  qWarning("SqliteResult::data latin1: " + valorLatin1 + ", data utf8:" + valorUTF8);
+  //qWarning("SqliteResult::data latin1: " + valorLatin1 + ", data utf8:" + valorUTF8);
   
   QVariant v = QVariant(valorLatin1);
   if (v.toString().isEmpty()) {
