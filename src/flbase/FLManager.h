39c4536f560568cd966f5a89e23e0a63829d1fe8
José A. Fernández <aullasistemas@gmail.com>
2024-11-24 12:04:45 +0100

    cachelite controla fllarge(12)

--- src/flbase/FLManager.h
+++ src/flbase/FLManager.h
@@ -383,7 +383,7 @@ public:
     return initCount_;
   }
 
-  FLTableMetaData checkFLLarge(const QString &tableLarge);
+  FLTableMetaData *checkFLLarge(const QString &tableLarge);
 
   bool initCacheLite(bool force = false);
 
