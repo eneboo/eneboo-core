9f38f11f45b4e44ca35f46a6d64a2c95c7b6e518
José A. Fernández <aullasistemas@gmail.com>
2024-11-24 10:07:38 +0100

    cachelite controla fllarge(5)

--- src/flbase/FLManager.cpp
+++ src/flbase/FLManager.cpp
@@ -1722,7 +1722,7 @@ QString tableLarge;
 
   tableLarge = use_cache_lite ? tableLarge + "_cachelite" : tableLarge;
 
-  QSqlQuery qryLarge(QString::null, use_cache_lite ? FLSqlConnections::database('cachelite')->db() : db_->db());
+  QSqlQuery qryLarge(use_cache_lite ? FLSqlConnections::database('cachelite')->db() : db_->db());
   if (qryLarge.exec(QString::fromLatin1("SELECT contenido FROM ") + tableLarge +
                     QString::fromLatin1(" WHERE refkey='") + refKey + QString::fromLatin1("'")) &&
       qryLarge.next()) {
