8d5805fe7331f3c7759c2156367d5f26a5d46b40
José A. Fernández <aullasistemas@gmail.com>
2025-03-06 14:22:01 +0100

    prueba

--- src/sqlapi/sqlitedataset.cpp
+++ src/sqlapi/sqlitedataset.cpp
@@ -664,8 +664,9 @@ namespace dbiplus
     
     if (fi_salida.open(IO_ReadOnly)) {
       QTextStream t(&fi_salida);
-      t.setCodec(QTextCodec::codecForName("ISO8859-15", 0));
-      salida = QString::fromLatin1(t.read());
+      //t.setCodec(QTextCodec::codecForName("ISO8859-15", 0));
+      t.setEncoding(QTextStream::Latin1);
+      salida = t.read();
       fi_salida.close();
 
       leido = true;
