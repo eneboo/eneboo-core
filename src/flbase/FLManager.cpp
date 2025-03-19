ac2acd7cebd1779c2638d6a0fd6c5920cd3fc231
José A. Fernández <aullasistemas@gmail.com>
2025-03-19 14:16:25 +0100

    mas debug

--- src/flbase/FLManager.cpp
+++ src/flbase/FLManager.cpp
@@ -712,6 +712,9 @@ FLTableMetaData *FLManager::metadata(QDomElement *mtd, bool quick)
           continue;
         }
         qWarning("W1 " + field.lower());
+        if (fields.find(field.lower()) >= 0) {
+          continue;
+        }
 
         FLTableMetaData *mtdAux = metadata(table, true);
         if (mtdAux) {
