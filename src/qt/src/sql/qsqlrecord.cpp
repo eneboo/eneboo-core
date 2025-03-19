e6a422b39f8977c5576435448d7e068fd0545432
José A. Fernández <aullasistemas@gmail.com>
2025-03-19 11:13:35 +0100

    prueba debugs

--- src/qt/src/sql/qsqlrecord.cpp
+++ src/qt/src/sql/qsqlrecord.cpp
@@ -294,7 +294,8 @@ QString QSqlRecord::fieldName( int i ) const
 int QSqlRecord::position( const QString& name ) const
 {
     for ( uint i = 0; i < count(); ++i ) {
-	if ( fieldName(i).upper() == name.upper() )
+        qWarning("Comparando " + fieldName(i).upper() + " con " + name.upper());
+	if ( fieldName(i).upper() == name.upper() || fieldName(i).upper().endsWith("." + name.upper()))
 	    return i;
     }
 #ifdef QT_CHECK_RANGE
