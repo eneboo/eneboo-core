f6e1cf50ce6b2f7b1e482a8df22a4a59f894f333
José A. Fernández <aullasistemas@gmail.com>
2025-04-08 10:39:24 +0200

    mappingValue(3)

--- src/flbase/FLFieldDB.cpp
+++ src/flbase/FLFieldDB.cpp
@@ -2497,7 +2497,7 @@ void FLFieldDB::setMapValue()
   if (!field || !fieldSender)
     return;
   if (cursor_->db()->driverName() == "FLsqlapi") {
-    qWarning("Checking previously mapping");
+    qWarning("Checking previously mapping. PREV:" + (mappingValue_ ? "TRUE" : "FALSE"));
     while (mappingValue_) {
         qApp->processEvents();
     }
