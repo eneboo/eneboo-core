e03acc2fb5e8cdcf74e062b5d1a0ebe4c11197a8
José A. Fernández <aullasistemas@gmail.com>
2025-04-16 15:40:52 +0200

    pruebas

--- src/libpq/fe-exec.c
+++ src/libpq/fe-exec.c
@@ -1252,8 +1252,8 @@ PQexec(PGconn *conn, const char *query)
 
 int max_retries = conn->connect_timeout == "0" ? 5 : 1;
 int retries = 0;
-fprintf("A: %s", conn->connect_timeout == '0' ? "1" : "0");
-fprintf("B: %s", conn->connect_timeout == '\0'? "1" : "0");
+fprintf(stdout,"A: %s (%s)", conn->connect_timeout == '0' ? "1" : "0", conn->connect_timeout);
+fprintf(stdout,"B: %s", conn->connect_timeout == '\0'? "1" : "0");
 
 #ifdef FL_SQL_LOG
 fprintf(stdout,"********* POSTGRESQL %s (%s)*********\n", conn->connect_timeout == '\0' ? "OLULA " : "", conn->connect_timeout);
