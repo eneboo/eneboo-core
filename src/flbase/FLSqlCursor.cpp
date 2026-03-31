7a630316848811b65d48226e0f4af73d81312698
José A. Fernández <aullasistemas@gmail.com>
2026-03-31 12:42:54 +0200

    debugs

--- src/flbase/FLSqlCursor.cpp
+++ src/flbase/FLSqlCursor.cpp
@@ -1615,7 +1615,7 @@ void FLSqlCursor::setValueBuffer(const QString &fN, const QVariant &v)
   }
   else
     d->buffer_->setValue(fN, vv);
-  qWarning("Emitiendo bufferChange por setValueBuffer de " d->metadata_->name() + "." + fN + ", valor: " + vv.toString());
+  qWarning("Emitiendo bufferChange por setValueBuffer de " + d->metadata_->name() + "." + fN + ", valor: " + QString(vv));
   emit bufferChanged(fN);
 }
 
