4f1416bb45e923e24730068ca266175e4044dd5f
José A. Fernández <aullasistemas@gmail.com>
2026-03-31 12:42:05 +0200

    debugs

--- src/flbase/FLFieldDB.cpp
+++ src/flbase/FLFieldDB.cpp
@@ -1825,7 +1825,7 @@ void FLFieldDB::refreshQuick(const QString &fN)
     return;
 
   QVariant v(cursor_->valueBuffer(fieldName_));
-  qWarning("FLFieldDB : " + "Valor fN " + fN.lower() + " = " + v.toString());
+  qWarning("FLFieldDB Valor fN " + fN.lower() + " = " + QString(v));
   bool null = cursor_->bufferIsNull(fieldName_);
   int partDecimal = partDecimal_ != -1 ? partDecimal_ : field->partDecimal();
   bool ol = field->hasOptionsList();
