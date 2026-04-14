ce3b08848c1b8aee5ec74e881aef7ff45e003784
José A. Fernández <aullasistemas@gmail.com>
2026-04-14 13:54:45 +0200

    fix SCRAM (17)

--- src/libpq/scram-common.c
+++ src/libpq/scram-common.c
@@ -20,11 +20,35 @@
 #include <stdlib.h>
 
 #include <openssl/hmac.h>
+#include <openssl/err.h>
 
 #include "scram_crypto.h"
 
 /* SCRAM-SHA-256 output length */
 #define SCRAM_KEY_LEN				PG_SHA256_DIGEST_LENGTH
+
+#ifdef SCRAM_DEBUG
+#define SCRAM_COMMON_LOG(fmt, ...) fprintf(stderr, "[SCRAM-COMMON] " fmt "\n", ##__VA_ARGS__)
+#else
+#define SCRAM_COMMON_LOG(fmt, ...) ((void)0)
+#endif
+
+static void
+scram_log_openssl_error(const char *where)
+{
+	unsigned long errcode;
+	char errbuf[256];
+
+	errcode = ERR_get_error();
+	if (errcode == 0)
+	{
+		SCRAM_COMMON_LOG("%s: OpenSSL returned failure without ERR_get_error()", where);
+		return;
+	}
+
+	ERR_error_string_n(errcode, errbuf, sizeof(errbuf));
+	SCRAM_COMMON_LOG("%s: OpenSSL error=%s", where, errbuf);
+}
 
 /*
  * scram_H
@@ -118,6 +142,9 @@ scram_SaltedPassword(const char *password,
 	if (saltlen > 1024)
 		return -1;
 
+	SCRAM_COMMON_LOG("scram_SaltedPassword: saltlen=%d iterations=%d plen=%d",
+					 saltlen, iterations, plen);
+
 	/* Build salt || INT(1) */
 	memcpy(saltbuf, salt, saltlen);
 	saltbuf[saltlen + 0] = 0;
@@ -129,17 +156,40 @@ scram_SaltedPassword(const char *password,
 #if OPENSSL_VERSION_NUMBER >= 0x10100000L
 	hctx = HMAC_CTX_new();
 	if (!hctx)
+	{
+		SCRAM_COMMON_LOG("scram_SaltedPassword: HMAC_CTX_new failed");
+		scram_log_openssl_error("HMAC_CTX_new");
 		return -1;
+	}
 #else
 	HMAC_CTX_init(hctx);
 #endif
 
 	/* U1 = HMAC(password, salt || INT(1)) */
-	if (HMAC_Init_ex(hctx, password, plen, EVP_sha256(), NULL) <= 0 ||
-		HMAC_Update(hctx, saltbuf, (size_t)(saltlen + 4)) <= 0 ||
-		HMAC_Final(hctx, Ui_prev, &outlen) <= 0 ||
-		outlen != SCRAM_KEY_LEN)
+	if (HMAC_Init_ex(hctx, password, plen, EVP_sha256(), NULL) <= 0)
+	{
+		SCRAM_COMMON_LOG("scram_SaltedPassword: HMAC_Init_ex failed in U1");
+		scram_log_openssl_error("HMAC_Init_ex U1");
 		goto fail;
+	}
+	if (HMAC_Update(hctx, saltbuf, (size_t)(saltlen + 4)) <= 0)
+	{
+		SCRAM_COMMON_LOG("scram_SaltedPassword: HMAC_Update failed in U1");
+		scram_log_openssl_error("HMAC_Update U1");
+		goto fail;
+	}
+	if (HMAC_Final(hctx, Ui_prev, &outlen) <= 0)
+	{
+		SCRAM_COMMON_LOG("scram_SaltedPassword: HMAC_Final failed in U1");
+		scram_log_openssl_error("HMAC_Final U1");
+		goto fail;
+	}
+	if (outlen != SCRAM_KEY_LEN)
+	{
+		SCRAM_COMMON_LOG("scram_SaltedPassword: unexpected U1 outlen=%u expected=%d",
+						 outlen, SCRAM_KEY_LEN);
+		goto fail;
+	}
 
 	memcpy(output, Ui_prev, SCRAM_KEY_LEN);
 
@@ -147,17 +197,38 @@ scram_SaltedPassword(const char *password,
 	for (i = 2; i <= iterations; i++)
 	{
 		/* Reinitialise with same key: pass NULL for md to reuse algorithm */
-		if (HMAC_Init_ex(hctx, password, plen, EVP_sha256(), NULL) <= 0 ||
-			HMAC_Update(hctx, Ui_prev, SCRAM_KEY_LEN) <= 0 ||
-			HMAC_Final(hctx, Ui, &outlen) <= 0 ||
-			outlen != SCRAM_KEY_LEN)
+		if (HMAC_Init_ex(hctx, password, plen, EVP_sha256(), NULL) <= 0)
+		{
+			SCRAM_COMMON_LOG("scram_SaltedPassword: HMAC_Init_ex failed at iteration=%d", i);
+			scram_log_openssl_error("HMAC_Init_ex Ui");
 			goto fail;
+		}
+		if (HMAC_Update(hctx, Ui_prev, SCRAM_KEY_LEN) <= 0)
+		{
+			SCRAM_COMMON_LOG("scram_SaltedPassword: HMAC_Update failed at iteration=%d", i);
+			scram_log_openssl_error("HMAC_Update Ui");
+			goto fail;
+		}
+		if (HMAC_Final(hctx, Ui, &outlen) <= 0)
+		{
+			SCRAM_COMMON_LOG("scram_SaltedPassword: HMAC_Final failed at iteration=%d", i);
+			scram_log_openssl_error("HMAC_Final Ui");
+			goto fail;
+		}
+		if (outlen != SCRAM_KEY_LEN)
+		{
+			SCRAM_COMMON_LOG("scram_SaltedPassword: unexpected outlen=%u at iteration=%d expected=%d",
+							 outlen, i, SCRAM_KEY_LEN);
+			goto fail;
+		}
 
 		for (j = 0; j < SCRAM_KEY_LEN; j++)
 			output[j] ^= Ui[j];
 
 		memcpy(Ui_prev, Ui, SCRAM_KEY_LEN);
 	}
+
+	SCRAM_COMMON_LOG("scram_SaltedPassword: completed successfully");
 
 #if OPENSSL_VERSION_NUMBER >= 0x10100000L
 	HMAC_CTX_free(hctx);
